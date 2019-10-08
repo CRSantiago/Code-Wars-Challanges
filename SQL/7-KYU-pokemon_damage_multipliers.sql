687ffe36acd3dd91cdbffd885cb60430e86b1193
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-10-08 08:41:45 -0400

    Add files via upload

---
+++ SQL/7-KYU-pokemon_damage_multipliers.sql
@@ -0,0 +1,24 @@
+-- You have arrived at the Celadon Gym to battle Erika for the Rainbow Badge.
+
+-- She will be using Grass-type Pokemon. Any fire pokemon you have will be strong against grass, but your water types will be weakened. The multipliers table within your Pokedex will take care of that.
+
+-- Using the following tables, return the pokemon_name, modifiedStrength and element of the Pokemon whose strength, after taking these changes into account, is greater than or equal to 40, ordered from strongest to weakest.
+
+-- pokemon schema
+
+-- id
+-- pokemon_name
+-- element_id
+-- str
+-- multipliers schema
+
+-- id
+-- element
+-- multiplier
+
+SELECT pokemon.pokemon_name, pokemon.str * multipliers.multiplier AS modifiedstrength, multipliers.element
+FROM pokemon
+JOIN multipliers ON pokemon.element_id=multipliers.id
+WHERE pokemon.str * multipliers.multiplier >= 40
+ORDER BY pokemon.str * multipliers.multiplier DESC
+;
