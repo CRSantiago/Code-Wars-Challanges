606682fd398bc21f97cc8dec58f3c022c4a0de6e
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2020-02-06 15:18:55 -0500

    Add files via upload

---
+++ Python/6-KYU-Counting-Duplicates.py
@@ -0,0 +1,24 @@
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+import string
+def duplicate_count(text):
+    letter_mapping = dict(zip(string.ascii_lowercase, [0]*26))
+    count = 0
+    for char in text.lower():
+        letter_mapping[char] += 1   
+    for v in letter_mapping.values():
+       if v > 1:
+           count += 1
+    return count
+
+print(duplicate_count('aabBcde'))
